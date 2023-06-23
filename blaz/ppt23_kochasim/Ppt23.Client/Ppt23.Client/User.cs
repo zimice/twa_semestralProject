8b938d7e6d5a0cee2b2455326fb12755308904e5
zimice <simon.kochanek@me.com>
2023-06-23 07:17:31 +0200

    commiting everything

---
+++ blaz/ppt23_kochasim/Ppt23.Client/Ppt23.Client/User.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ppt23.Client
+{
+    public class User
+    {
+
+        public int UserID { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public DateTime CreatedAt { get; set; } 
+        public User(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+        }
+    }
+}
